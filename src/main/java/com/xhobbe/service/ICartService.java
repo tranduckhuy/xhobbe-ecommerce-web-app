88d6fabbb65d02c8ad1208b10d1cdcf6799aad16
TheCollector009 <hoangxd3549@gmail.com>
2023-11-20 11:06:57 +0700

    Add CartService

--- src/main/java/com/xhobbe/service/ICartService.java
+++ src/main/java/com/xhobbe/service/ICartService.java
@@ -1,10 +1,23 @@
-
 package com.xhobbe.service;
+
+import com.xhobbe.model.Cart;
+import java.util.List;
 
 /**
  *
  * @author ADMIN
  */
 public interface ICartService {
-    
+
+    Long add(Cart cart);
+
+    List<Cart> findAll(int limit, int offset, String orderBy, String sortBy);
+
+    List<Cart> findByUserId(long userId);
+
+    void updateQuantity(int quantity, long cartId);
+
+    void delete(long id);
+
+    int getTotalItem();
 }
--- src/main/java/com/xhobbe/service/impl/CartService.java
+++ src/main/java/com/xhobbe/service/impl/CartService.java
@@ -1,12 +1,48 @@
-
 package com.xhobbe.service.impl;
 
+import com.xhobbe.dao.ICartDAO;
+import com.xhobbe.model.Cart;
 import com.xhobbe.service.ICartService;
+import java.util.List;
+import javax.inject.Inject;
 
 /**
  *
  * @author ADMIN
  */
 public class CartService implements ICartService {
-    
+
+    @Inject
+    ICartDAO cartDAO;
+
+    @Override
+    public Long add(Cart cart) {
+        return cartDAO.add(cart);
+    }
+
+    @Override
+    public List<Cart> findAll(int limit, int offset, String orderBy, String sortBy) {
+        return cartDAO.findAll(limit, offset, orderBy, sortBy);
+    }
+
+    @Override
+    public List<Cart> findByUserId(long userId) {
+        return cartDAO.findByUserId(userId);
+    }
+
+    @Override
+    public void updateQuantity(int quantity, long cartId) {
+        cartDAO.updateQuantity(quantity, cartId);
+    }
+
+    @Override
+    public void delete(long id) {
+        cartDAO.delete(id);
+    }
+
+    @Override
+    public int getTotalItem() {
+        return cartDAO.getTotalItem();
+    }
+
 }
